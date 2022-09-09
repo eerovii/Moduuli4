1791324752ac5b0fb1f1cd3ffdf899852a75e330
eerovii <111981034+eerovii@users.noreply.github.com>
2022-09-09 09:24:33 +0300

    Update Moduuli 4.2.py

--- Moduuli 4.2.py
+++ Moduuli 4.2.py
@@ -2,4 +2,5 @@ tuuma=float(input('Anna mitta tuumina: '))
 while tuuma>-1:
     tuuma=float(input('Anna mitta tuumina: '))
     if tuuma>=0:
+        #tuuman muunnos senttimetreiksi
         print(str(tuuma*2.54) + 'cm')
