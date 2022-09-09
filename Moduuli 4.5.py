ea91fa4ca088c079c2ef78b592e98c0052396803
eerovii <111981034+eerovii@users.noreply.github.com>
2022-09-09 09:25:26 +0300

    Update Moduuli 4.5.py

--- Moduuli 4.5.py
+++ Moduuli 4.5.py
@@ -1,7 +1,9 @@
 tries=0
 while tries<5 and (input('Käyttäjä: ')!='python' or input('Salasana: ')!='rules'):
+    #määritetyt läpipääsyn ehdot
     tries+=1
 if tries<5:
+    #yrityskerrat
     print('Tervetuloa!')
 else:
     print('Pääsy evätty.')
